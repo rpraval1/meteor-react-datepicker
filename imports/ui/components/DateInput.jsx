899a6c9b4852205db9d732b5f2ba5d0aa7b86b66
rpraval1 <rpraval1@binghamton.edu>
2017-03-08 14:59:24 -0500

    created DateInput component to let user pick a date save date

--- imports/ui/components/DateInput.jsx
+++ imports/ui/components/DateInput.jsx
@@ -1,23 +1,39 @@
 import React, { Component } from 'react'
 import { Meteor } from 'meteor/meteor';
 import { Button, Container, Header, Form, Icon, Input, Segment } from 'semantic-ui-react'
+import DatePicker from 'react-datepicker'
+import moment from 'moment';
 import { Dates } from '../../collections/Dates'
-
+require('react-datepicker/dist/react-datepicker.css');
 
 class DateInput extends Component{
   constructor(props){
     super(props);
     this.state = {
-
+      inputDate: moment()
     };
   }
 
+  handleinputDate(date){
+    this.setState({
+      inputDate: date
+    })
+  }
 
+  saveDate(){
+    Meteor.call('date.save', this.refs.myDate.value, (error, result) => {
+      if(error){
+        console.log(error);
 
-
+      }else{
+        console.log(result);
+        console.log("date inserted");
+      }
+    })
+  }
 
   render(){
-
+    const {inputDate} = this.state
     return(
       <Container>
         <Header as='h3'>
@@ -27,6 +43,11 @@ class DateInput extends Component{
           </Header.Content>
         </Header>
         <Segment raised>
+          <DatePicker
+            selected={inputDate}
+            ref= 'myDate'
+            onChange={this.handleinputDate.bind(this)} />
+          <Button onClick={this.saveDate.bind(this)}>Add</Button>
         </Segment>
       </Container>
     );
